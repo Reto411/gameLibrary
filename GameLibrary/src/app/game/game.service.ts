1f960c5505176b4de2f3d13c24653cf68ce2b1e1
Reto411 <buergissera@eggrain.ch>
2017-06-17 08:31:11 -0700

    Add bootstrap formatings

--- GameLibrary/src/app/game/game.service.ts
+++ GameLibrary/src/app/game/game.service.ts
@@ -2,9 +2,14 @@ import { Injectable } from '@angular/core';
 import { Game } from './game.model';
 
 const GAMESMOCKS: Game[] = [
-  {id: 1, name:"Crusader Kings II"},
-  {id: 2, name:"Halo Reach"},
-  {id: 3, name:"League of Faggots"}
+  {id: 1, name: "Crusader Kings II"},
+  {id: 2, name: "Halo Reach"},
+  {id: 3, name: "League of Faggots"},
+  {id: 4, name: "Red Orchestra II: Eastern Front"},
+  {id: 5, name: "PLAYERSUNKNOWNS BATTLEGROUND"},
+  {id: 6, name: "Stardew Valley"},
+  {id: 7, name: "Stronghold HD"},
+  {id: 8, name: "The Witcher 3: Wild Hunt"}
 ];
 
 @Injectable()
