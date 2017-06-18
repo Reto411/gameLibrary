9787434bacfad4a6560499716f71a65ba003d868
Reto411 <buergissera@eggrain.ch>
2017-06-18 11:24:22 -0700

    Add travis

--- GameLibrary/src/app/shared/services/game.service.ts
+++ GameLibrary/src/app/shared/services/game.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Game } from '../models/game.model';
 import {Picture} from "../models/picture.model";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 
 
 
 
 @Injectable()
 export class GameService {
-  getGames(): Promise<Game[]> {
+  constructor( private http: Http)
+  {
 
+  }
+
+  getGames(): Observable<Game[]> {
+    return this.http.get("../assets/games/statig.game.data.json")
   }
 }
